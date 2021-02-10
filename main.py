94c1c72621c2c48692b2ace5c3ce963c1462ffe7
Sattwik Sahu <sahusattwik@gmail.com>
2021-02-10 18:34:09 +0530

    Minor bug fixes

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ def add_new_supplement():
     supplement_name = input("Enter supplement name :")
     cost = int(input("Enter cost of supplement :"))
     category = input("Enter category of supplement:")
-    bdf = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+    bdf = pd.read_csv("Supplements.csv")
     n = bdf["supplement_id"].count()
     bdf.at[n] = [supplement_id, supplement_name, cost, category]
-    bdf.to_csv(r"file:///D:/IP/Supplements.csv", index=False)
+    bdf.to_csv("Supplements.csv", index=False)
     print("Supplement Added Successfully :)")
     print(bdf)
 
 
 def search_supplement():
     supplement_name = input("Enter a Supplement name:")
-    bdf = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+    bdf = pd.read_csv(r"Supplements.csv")
     df = bdf.loc[bdf["supplement_name"] == supplement_name]
     if df.empty:
         print("No supplement found with given code  :(")
@@ -31,15 +31,15 @@ def search_supplement():
 
 def delete_supplement():
     supplement_id = float(input("Enter supplement_id:"))
-    bdf = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+    bdf = pd.read_csv(r"Supplements.csv")
     bdf = bdf.drop(bdf[bdf["supplement_id"] == supplement_id].index)
-    bdf.to_csv(r"file:///D:/IP/Supplements.csv", index=False)
+    bdf.to_csv(r"Supplements.csv", index=False)
     print("Supplement Deleted Successfully ;) ")
     print(bdf)
 
 
 def show_supplement():
-    bdf = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+    bdf = pd.read_csv(r"Supplements.csv")
     print(bdf)
 
 
@@ -48,17 +48,17 @@ def add_new_player():
     name = input("Enter player name :")
     stage = input("Enter players level:")
     year_of_joining = input("Enter joining Year:")
-    bdf = pd.read_csv(r"file:///D:/IP/player.csv")
+    bdf = pd.read_csv(r"player.csv")
     n = bdf["player_id"].count()
     bdf.at[n] = [player_id, name, stage, year_of_joining]
-    bdf.to_csv(r"file:///D:/IP/player.csv", index=False)
+    bdf.to_csv(r"player.csv", index=False)
     print("Player added successfully")
     print(bdf)
 
 
 def search_player():
     name = input("Enter player Name:")
-    bdf = pd.read_csv(r"file:///D:/IP/player.csv")
+    bdf = pd.read_csv(r"player.csv")
     df = bdf.loc[bdf["player_name"] == name]
     if df.empty:
         print("No player Found")
@@ -69,39 +69,40 @@ def search_player():
 
 def delete_player():
     player_id = float(input("Enter Player_id:"))
-    bdf = pd.read_csv(r"file:///D:/IP/player.csv")
+    bdf = pd.read_csv(r"player.csv")
     bdf = bdf.drop(bdf[bdf["player_id"] == player_id].index)
-    bdf.to_csv(r"file:///D:/IP/player.csv", index=False)
+    bdf.to_csv(r"player.csv", index=False)
     print("Player Deleted Successfully")
     print(bdf)
 
 
 def show_player():
-    bdf = pd.read_csv(r"file:///D:/IP/player.csv")
+    bdf = pd.read_csv(r"player.csv")
     print(bdf)
 
 
 def issue_supplements():
     supplement_name = input("Enter supplement name:")
-    bdf = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+    bdf = pd.read_csv(r"Supplements.csv")
     bdf = bdf.loc[bdf["supplement_name"] == supplement_name]
     if bdf.empty:
         print("No Supplement found:(")
         return
 
     p_name = input("Enter player name :")
-    mdf = pd.read_csv(r"file:///D:/IP/player.csv")
-    mdf = mdf.loc[mdf["player_name "] == p_name]
+    mdf = pd.read_csv(r"player.csv")
+    mdf = mdf.loc[mdf["player_name"] == p_name]
     if mdf.empty:
         print("No Such Member found:(")
         return
 
-    delete_of_issue = int(input("Enter Date of issue:"))
-    number_of_supplements_issued = int(input("enter  number of book issued:"))
-    bdf = pd.read_csv(r"file:///D:/IP/issue.csv")
+    # delete_of_issue = input("Enter Date of issue:")
+    number_of_supplements_issued = int(input("enter  number of supplements issued:"))
+    bdf = pd.read_csv(r"issue.csv")
     n = bdf["supplement_name"].count()
-    bdf.at[n] = [supplement_name, p_name, date.today(), number_of_supplements_issued, ""]
-    bdf.to_csv(r"file:///D:/IP/issue.csv", index=False)
+    bdf.at[n] = [supplement_name, p_name,
+                 date.today(), number_of_supplements_issued, ""]
+    bdf.to_csv(r"issue.csv", index=False)
     print("Supplement issued successfully")
     print(bdf)
 
@@ -109,7 +110,7 @@ def issue_supplements():
 def return_supplement():
     p_name = input("Enter a player name : ")
     supplement_name = input("Enter supplement Name :")
-    idf = pd.read_csv(r"file:///D:/IP/issue.csv")
+    idf = pd.read_csv(r"issue.csv")
     idf = idf.loc[idf["supplement_name"] == supplement_name]
     if idf.empty:
         print("The supplement is not issued in the Arsenal")
@@ -121,24 +122,25 @@ def return_supplement():
             print("Supplement can be Returned ;)")
             ans = input("Are you sure you want to return the supplement ")
             if ans.lower() == "yes":
-                idf = pd.read_csv(r"file:///D:/IP/issue.csv")
-                idf = idf.drop(idf[idf["supplement_name"] == supplement_name].index)
-                idf.to_csv(r"file:///D:/IP/issue.csv", index=False)
+                idf = pd.read_csv(r"issue.csv")
+                idf = idf.drop(idf[idf["supplement_name"]
+                                   == supplement_name].index)
+                idf.to_csv(r"issue.csv", index=False)
                 print("Supplement Received successfully :)")
             else:
                 print("Return operation Unsuccessful")
 
 
 def show_issued_supplements():
-    idf = pd.read_csv(r"file:///D:/IP/issue.csv")
+    idf = pd.read_csv(r"issue.csv")
     print(idf)
 
 
 def delete_issued_supplement():
     supplement_name = input("Enter Supplement Name : ")
-    bdf = pd.read_csv(r"file:///D:/IP/issue.csv")
+    bdf = pd.read_csv(r"issue.csv")
     bdf = bdf.drop(bdf[bdf["supplement_name"] == supplement_name].index)
-    bdf.to_csv(r"file:///D:/IP/issue.csv", index=False)
+    bdf.to_csv(r"issue.csv", index=False)
     print("Deleted issued Supplement successfully :) ")
     print(bdf)
 
@@ -148,24 +150,24 @@ def show_charts():
     print("Press 2 - Number of Supplements issued by members ")
     char = int(input("Enter you choice: "))
     if char == 1:
-        df = pd.read_csv(r"file:///D:/IP/Supplements.csv")
+        df = pd.read_csv(r"Supplements.csv")
         df = df[["supplement_name", "cost"]]
         df.plot("supplement_name", "cost", kind='bar')
         plt.xlabel('supplement_name---->')
         plt.ylabel('cost---->')
         plt.show()
 
-    if ch == 2:
-        df = pd.read_csv(r"file:///D:/IP/issue.csv")
-        df = df[["number of supplements issued"]]
-        df.plot(kind='bar', colour='red')
+    if char == 2:
+        df = pd.read_csv("issue.csv")
+        df = df["number_of_supplements"]
+        df.plot(kind='bar', color='red')
         plt.show()
 
 
 def login():
     uname = input("Enter your name : ")
     pwd = input("Enter your password : ")
-    df = pd.read_csv(r"file:///D:/IP/user.csv")
+    df = pd.read_csv(r"user.csv")
     df = df.loc[df["uname"] == uname]
     if df.empty:
         print("Invalid User Name given :( ")
@@ -241,3 +243,4 @@ if login():
             print("Invalid Option selected :( ")
 
 print("*****THANK YOU FOR VISITING ACE SPORTS CLUB*****")
+print(f"Don't forget to give a star on GitHub https://github.com/Suave-creates/Sports-Club-Management/")
